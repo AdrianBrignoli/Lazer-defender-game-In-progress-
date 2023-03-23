3970fca8b61b24ba091ea4cdb078259c7941cfb9
AdrianBrignoli <65016196+AdrianBrignoli@users.noreply.github.com>
2023-03-23 10:31:44 +0100

    Add files via upload
    Scripts

---
+++ EnemyPathing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    WaveConfig waveConfig;
+    List<Transform> waypoints;
+    int waypointsIndex = 0;
+
+    private void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointsIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
+    private void Move()
+    {
+        if (waypointsIndex <= waypoints.Count - 1)
+        {
+            // Where we want to go to
+            var targetPosition = waypoints[waypointsIndex].transform.position;
+            // Speed getting there
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+
+            // Check if we've reached the target
+            if (transform.position == targetPosition)
+            {
+                waypointsIndex++;
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
